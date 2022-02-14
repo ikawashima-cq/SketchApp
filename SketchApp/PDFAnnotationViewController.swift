6cffa061f203e16c6792101d7232f312b6042894
msugai-cq <m.sugai@caraquri.com>
2022-02-14 18:14:37 +0900

    注釈サンプル

--- SketchApp/PDFAnnotationViewController.swift
+++ SketchApp/PDFAnnotationViewController.swift
@@ -45,6 +45,16 @@ class PDFAnnotationViewController: UIViewController, PDFViewDelegate {
         self.pdfView = pdfView
         self.pdfDocument = document
         //let highLightItem = UIMenuItem(title:"Highlight"), action: #selector(highlightFromMenuItem))
+
+
+        let button = UIButton()
+        button.frame = CGRect(x: 0, y: 20, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height / 8)
+        button.setTitle("Add annotate", for: .normal)
+        button.backgroundColor = UIColor.black
+        button.addTarget(self, action: #selector(addAnnotate), for: .touchUpInside)
+        self.view.addSubview(button)
+
+        self.setupNotification()
     }
 
     @objc func highlightFromMenuItem() {
@@ -58,5 +68,31 @@ class PDFAnnotationViewController: UIViewController, PDFViewDelegate {
                 self.pdfView?.currentPage?.addAnnotation(annotation)
             })
         }
+    }
+
+    @objc func addAnnotate() {
+        let annotation = PDFAnnotation(bounds: CGRect(x: 200, y: 400, width: 200, height: 50), forType: .freeText, withProperties: nil)
+        annotation.contents = "PDFAnnotation"
+        annotation.font = .systemFont(ofSize: 14)
+        annotation.fontColor = .red
+        annotation.color = .yellow
+
+        // 対象のPDFPageへPDFAnnotationを設定する
+        pdfView.currentPage?.addAnnotation(annotation)
+    }
+
+    private func setupNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(didTapAnnotation(_:)), name: .PDFViewAnnotationHit, object: nil)
+    }
+
+    @objc func didTapAnnotation(_ notification: Notification) {
+        // タップされたPDFAnnotationを取得する
+        guard
+            let annotation = notification.userInfo?["PDFAnnotationHit"] as? PDFAnnotation
+        else {
+            return
+        }
+
+        annotation.contents = "TAPPED"
     }
 }
